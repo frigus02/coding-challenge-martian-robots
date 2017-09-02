0f7d82a30217c873eadbfc5d4c3207d0b51b2dd3
Jan Kuehle <jkuehle90@gmail.com>
2017-09-02 10:44:08 +0200

    Add basic structure and tests based on provided sample

---
+++ test/samples.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+
+const program = require('../src/index');
+
+const samplePath = 'test/samples/';
+
+describe('Samples', function () {
+    const files = fs.readdirSync(samplePath);
+    const inFiles = files.filter(name => name.endsWith('.in.txt'));
+
+    for (const file of inFiles) {
+        it(`should generate the correct output for ${file}`, async function () {
+            const input = await readFile(samplePath + file, 'utf-8');
+            const expectedOutput = await readFile(samplePath + file.replace('.in.txt', '.out.txt'), 'utf-8');
+
+            const output = program(input);
+
+            assert.equal(expectedOutput, output);
+        });
+    }
+});
